3b5dcd28d96f394c32ab735a5f3a75f99f5b1fa1
faridaalin <56302698+faridaalin@users.noreply.github.com>
2020-06-02 18:36:35 +0200

    new images, javascript, removed repeated code

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const mobileMenu = () => {
+    const hamburger = document.querySelector('#hamburger');
+    const ulContainer = document.querySelector('.box');
+
+    hamburger.addEventListener('click', () => {
+        ulContainer.classList.toggle('box-active')
+    })
+
+}
+mobileMenu()
+
+window.onload = changeImageOnLoad;
+
+const imgArray = ['../img/sunset.png', '../img/birdsview.png', '../img/branch.png'];
+const imgArrayLength = imgArray.length
+
+
+function changeImageOnLoad() {
+    const backgroundImg = document.querySelector('.bg-container')
+    let randomImg = Math.floor(Math.random() * imgArrayLength)
+
+    backgroundImg.style.backgroundImage = "url("+imgArray[randomImg] +")";
+}
+
+
+
+
